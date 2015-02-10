5a1494c520f6cda7ce30a686fedb87e9f9a2f8af
= <=>
2015-02-10 13:50:44 -0600

    DS2 week 1 prequiz

---
+++ DS2/age-gender.R
@@ -0,0 +1,3 @@
+structure(list(age = c(35, 35, 35, 35), gender = structure(c(2L, 
+1L, 2L, 1L), .Label = c("female", "male"), class = "factor")), .Names = c("age", 
+"gender"), row.names = c("subj1", "subj2", "subj3", "subj4"), class = "data.frame")
